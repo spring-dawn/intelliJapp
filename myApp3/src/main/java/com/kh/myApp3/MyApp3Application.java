1898dbb8abd02f640e12257e654d40096c646d58
Lariatte <one_spring_day94@naver.com>
2022-04-19 00:33:16 +0900

    220419 myApp3 시작

---
+++ myApp3/src/main/java/com/kh/myApp3/MyApp3Application.java
@@ -0,0 +1,13 @@
+package com.kh.myApp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApp3Application.class, args);
+	}
+
+}
---
+++ myApp3/src/test/java/com/kh/myApp3/MyApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.myApp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

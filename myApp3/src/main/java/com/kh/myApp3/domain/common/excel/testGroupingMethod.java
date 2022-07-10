45bf1fdfac86f452124b81c3a461a37d62937106
Lariatte <one_spring_day94@naver.com>
2022-07-10 21:56:55 +0900

    220710 클립보드 복사 기능 연습

--- myApp3/src/main/java/com/kh/myApp3/domain/common/excel/testGroupingMethod.java
+++ myApp3/src/main/java/com/kh/myApp3/domain/common/excel/testGroupingMethod.java
@@ -1,35 +1,35 @@
 package com.kh.myApp3.domain.common.excel;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
 public class testGroupingMethod {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>(Arrays.asList("Foo", "Bar", "Bar", "Bar", "Foo"));
+            int index = 0;
+            int[] array = new int[3];
+            array[0] = 2;
+            array[1] = 5;
+            array[2] = 8;
 
-        Map<String, Long> result = list.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            Loop1:
+            for (int i = 0; i < array.length; i++) {
+                for (int j = index; j < 10; j++) {
+                    if (array[i] == j) {
+                        System.out.println(array[i]);
+                        index = j + 1;
+                        continue Loop1;
+                    }
+                }
+            }
 
-        System.out.println(result.keySet());
-        System.out.println(result.get("Foo"));
-        System.out.println(result.get("Bar"));
+        }
 
 
-//        2)
-        List<String> list2 = new ArrayList<>(Arrays.asList("Adam", "Bill", "Jack", "Joe", "Ian"));
-
-        Map<Integer, Long> result2 = list.stream()
-                .collect(Collectors.groupingByConcurrent(String::length, Collectors.counting()));
-        System.out.println(result2.get(3));
-        System.out.println(result2.get(4));
-
-//        3)frequency 로 특정 문자열이 몇 개나 있는지 찾기
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 1, 1, 2);
-        System.out.println(Collections.frequency(numbers, 1));
-        
-        
-    }
 }
 
